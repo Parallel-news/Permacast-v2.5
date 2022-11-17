6c0214e38ed1cdf2e0e63df40dcbca21ebc8e80c
LwaziNF <lwazinf@gmail.com>
2022-11-17 09:26:42 +0200

    refactoring/cleanup :: home.jsx already neat, under pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from 'react';
-import { appContext } from '../utils/initStateGen.js';
-
-import { 
+import React, { useContext } from "react";
+import { appContext } from "../utils/initStateGen.js";
+import {
   Greeting,
   FeaturedEpisode,
   FeaturedPodcastsMobile,
-  FeaturedVideoShows,
   RecentlyAdded,
-  FeaturedCreators
-} from '../component/featured';
+  FeaturedCreators,
+} from "../component/featured";
 
 // var featuredVideoShows = [{
 // contentTx: null,
@@ -34,58 +32,50 @@ import {
 // visible: true
 // }]
 
-export default function Home({recentlyAdded, featuredPodcasts}) {
-  const appState = useContext(appContext)
-  const Loading = () => <div className="w-full h-[100px] rounded-3xl mt-2 animate-pulse bg-gray-300/30"></div>
+export default function Home({ recentlyAdded, featuredPodcasts }) {
+  const appState = useContext(appContext);
+  const Loading = () => (
+    <div className="w-full h-[100px] rounded-3xl mt-2 animate-pulse bg-gray-300/30"></div>
+  );
 
   return (
     <div className="overflow-scroll w-full pb-10 mb-10">
-      {!appState.loading ? (
-        <Greeting />
-      ): <Loading />}
+      {!appState.loading ? <Greeting /> : <Loading />}
       {!appState.loading ? (
         <div className="hidden md:block">
           <FeaturedEpisode episode={recentlyAdded[0]} episodeId={1} />
         </div>
-      ): <Loading />}
+      ) : (
+        <Loading />
+      )}
+
       {/* {!appState.loading ? (
         <div className="hidden md:grid w-full mt-8 grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-x-12">
           <FeaturedPodcasts podcasts={featuredPodcasts} />
         </div>
       ): <Loading />} */}
-      
-      {/* {!appState.loading ? (<div className={`w-full h-[25px] flex flex-row mt-3 ml-2`}>
-        <div className={`h-full min-w-[30px] rounded-[4px] flex flex-row justify-center items-center mx-1 cursor-pointer ${showPods_ ? 'bg-white/80 hover:bg-white/80' : 'bg-white/50 hover:bg-white/80'} transition-all duration-200`} onClick={() => {
-          setShowPods_(true)
-        }}>
-          <p className={`m-2 text-black/80 font-black`}>Podcasts</p>
-        </div>
-
-        <div className={`h-full min-w-[30px] rounded-[4px] flex flex-row justify-center items-center mx-1 cursor-pointer ${!showPods_ ? 'bg-white/80 hover:bg-white/80' : 'bg-white/50 hover:bg-white/80'} transition-all duration-200`} onClick={() => {
-          setShowPods_(false)
-        }}>
-          <p className={`m-2 text-black/80 font-black`}>Videos</p>
-        </div>
-      </div>) : <Loading />} */}
 
       {!appState.loading ? (
-        // showPods_ ?
-        <FeaturedPodcastsMobile podcasts={featuredPodcasts} /> 
-        // :
-        // <FeaturedVideoShows videoShows={[{}]} />
-      ): <Loading />}
+        <FeaturedPodcastsMobile podcasts={featuredPodcasts} />
+      ) : (
+        <Loading />
+      )}
       <div className="my-9 grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-x-12">
         <div className="xl:col-span-3 lg:col-span-2 md:col-span-1 mb-9">
           {!appState.loading ? (
             <RecentlyAdded episodes={recentlyAdded} />
-          ): <Loading />}
+          ) : (
+            <Loading />
+          )}
         </div>
         {!appState.loading ? (
           <div className="w-full">
             <FeaturedCreators />
           </div>
-        ): <Loading />}
+        ) : (
+          <Loading />
+        )}
       </div>
     </div>
-  )
+  );
 }
