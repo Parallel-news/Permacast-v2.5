78952d9fcb2f3bc303f2186a2fb092df301f7d74
LwaziNF <lwazinf@gmail.com>
2022-12-16 07:43:25 +0200

    ui :: Fullscreen api now compatible with exm api

--- src/component/fullscreen.jsx
+++ src/component/fullscreen.jsx
@@ -3,19 +3,28 @@ import { useLocation, useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { appContext } from '../utils/initStateGen.js';
+import { primaryData, secondaryData, switchFocus } from '../atoms/index.js';
+import { useRecoilState } from 'recoil';
 
 export default function Fullscreen({episode, number}) {
   const appState = useContext(appContext);
   const history = useHistory();
   const { t } = useTranslation();
 
+  const [switchFocus_, setSwitchFocus_] = useRecoilState(switchFocus);
+  const [primaryData_, setPrimaryData_] = useRecoilState(primaryData);
+  const [secondaryData_, setSecondaryData_] = useRecoilState(secondaryData);
+
   return (
     <div className="absolute h-full w-full z-[20] bg-black" onClick={console.log/*`url("${episode?.cover}")`}}> */}>
-      <img className="absolute h-full w-full blur-lg opacity-30 object-cover" src={episode?.cover} alt={episode?.title} />
+      <img className="absolute h-full w-full blur-lg opacity-30 object-cover" src={'https://arweave.net/'+secondaryData_.cover} alt={episode?.episodeName} />
       <div className="absolute mt-28 w-full text-center select-text">
-        <img className="w-[25%] cursor-pointer mx-auto" src={episode?.cover} alt={episode?.title} />
-        <div className="mt-5 text-3xl font-bold text-white">{t("fullscreen.episode")} {number}</div>
-        <div className="mt-5 text-xl text-gray-300">{episode?.title}</div>
+        <img className="w-[25%] cursor-pointer mx-auto" src={'https://arweave.net/'+secondaryData_.cover} alt={episode?.episodeName} />
+        <div className="mt-5 text-3xl font-bold text-white">{t("fullscreen.episode")} {secondaryData_
+    .episodes.findIndex((obj) => {
+      return obj.eid === number;
+    }) + 1} </div>
+        <div className="mt-5 text-xl text-gray-300">{episode?.episodeName}</div>
       </div>
     </div>
   )
