fef137f3a4fd334ca4416bb85208792e799508cb
Max Knivets <knivets.m@gmail.com>
2023-01-13 07:17:08 -0500

    fix: added dev contract, fix error repr, hook simplification

--- src/utils/ethereum.js
+++ src/utils/ethereum.js
@@ -1,14 +1,13 @@
 import { useSendTransaction, usePrepareSendTransaction, useAccount } from 'wagmi';
 import { parseEther } from 'ethers/lib/utils';
 
-export default function useEthTransactionHook(addressToSendTo, value) {
+export default function useEthTransactionHook() {
   // const { address, isConnecting, isDisconnected } = useAccount();
 
-  // example args: 'permacast.eth', '0.001'
   const { config, error } = usePrepareSendTransaction({
     request: {
-      to: addressToSendTo,
-      value: parseEther(value),
+      to: "0x197f818c1313DC58b32D88078ecdfB40EA822614",
+      value: parseEther('0.001'),
     },
   })
   const { data, isLoading, isSuccess,  sendTransaction } = useSendTransaction(config);
