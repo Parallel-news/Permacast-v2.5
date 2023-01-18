18c898271b70497982422bdff0b46deef77d02b2
Max Knivets <knivets.m@gmail.com>
2023-01-18 09:00:42 -0500

    🔝 types for api

--- src/pages/api/exm/read.ts
+++ src/pages/api/exm/read.ts
@@ -1,9 +1,17 @@
 import axios from "axios"
+import { NextApiRequest, NextApiResponse } from "next"
 
 const CONTRACT = process.env.EXM_CONTRACT_ADDRESS
 const DEV_CONTRACT = process.env.EXM_DEV_CONTRACT_ADDRESS
 
-export default async function handler(req, res) {
+interface ResponseData {
+  
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
   try {
     const data = await axios.get(`https://api.exm.dev/read/${CONTRACT}`)
     res.status(200).json(data.data)
