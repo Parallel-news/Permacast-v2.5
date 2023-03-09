56cbdda9e34e8f31dae3f877698361338308865b
Max Knivets <knivets.m@gmail.com>
2023-03-09 15:23:49 -0500

    fix: variable naming

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,12 +1,11 @@
-export const PROD_CONTRACT = process.env.EXM_CONTRACT_ADDRESS;
-export const DEV_CONTRACT = process.env.EXM_DEV_CONTRACT_ADDRESS;
-export const PROD_TOKEN = process.env.EXM_API_TOKEN;
-export const DEV_TOKEN = process.env.EXM_DEV_API_TOKEN;
-export const IS_PROD = process.env.IS_PROD;
-
 export const getContractVariables = () => {
-  const contractAddress = IS_PROD ? PROD_CONTRACT : DEV_CONTRACT;
-  const contractAPIToken = IS_PROD ? PROD_TOKEN : DEV_TOKEN;
-  console.log({ contractAddress, contractAPIToken })
+  const PROD_CONTRACT = process.env.EXM_PROD_CONTRACT_ADDRESS;
+  const DEV_CONTRACT = process.env.EXM_DEV_CONTRACT_ADDRESS;
+  const PROD_TOKEN = process.env.EXM_PROD_API_TOKEN;
+  const DEV_TOKEN = process.env.EXM_DEV_API_TOKEN;
+  const IS_PROD = process.env.IS_PROD;
+
+  const contractAddress = IS_PROD === 'true' ? PROD_CONTRACT : DEV_CONTRACT;
+  const contractAPIToken = IS_PROD === 'true' ? PROD_TOKEN : DEV_TOKEN;
   return { contractAddress, contractAPIToken };
 }
