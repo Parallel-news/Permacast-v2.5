432f823594f3d873a49abe0edd282b351e0e532b
godzilla1980 <godzilladino1980@gmail.com>
2023-03-14 10:17:22 -0500

    :hammer: add ServerSide Props to pid Page

--- src/component/TextTruncate.tsx
+++ src/component/TextTruncate.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FADE_IN_STYLE } from "../constants";
+import { useTranslation } from "next-i18next";
 
 
 interface TextTruncateProps {
@@ -13,8 +14,8 @@ const TextTruncate: React.FC<TextTruncateProps> = ({ text, limit, textClass, but
   const [showFullText, setShowFullText] = useState(false);
   const truncatedText = showFullText ? text : `${text.slice(0, limit)}${text.length < limit ? "" : "..."}`;
   const [isVisible, setIsVisible] = useState<boolean>(true)
-
-  const toggleShowFullText = () => {
+  const { t } = useTranslation()
+    const toggleShowFullText = () => {
     setIsVisible(false)
     const timeout = setTimeout(() => {
       setIsVisible(true)
@@ -27,7 +28,7 @@ const TextTruncate: React.FC<TextTruncateProps> = ({ text, limit, textClass, but
       <p className={textClass+ " " +(isVisible ? "opacity-100" : (FADE_IN_STYLE))}>{truncatedText}</p>
       {text.length > limit && (
         <button onClick={toggleShowFullText} className={buttonClass}>
-          {showFullText ? "Show Less" : "Show More"}
+          {showFullText ? t("navbar.home") : t("navbar.home")}
         </button>
       )}
     </div>
--- src/pages/podcast/[pid].tsx
+++ src/pages/podcast/[pid].tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useRecoilState } from "recoil";
 import { backgroundColor } from "../../atoms";
 import { 
@@ -5,16 +6,21 @@ import {
     podcastIdStyling,
 } from "../../component/episode/eidTools";
 import { PodcastBanner } from "../../component/podcast/pidTools";
+import { EXM_READ_LINK, ARWEAVE_READ_LINK } from "../../constants";
+import { getContractVariables } from "../../utils/contract";
+import { findObjectById } from "../../utils/reusables";
+import { formatStringByLen } from "../../utils/reusables";
 
-export default function PodcastId() {
+export default function PodcastId({data}) {
     const [backgroundColor_, setBackgroundColor_] = useRecoilState(backgroundColor);
     //State Calls Here
+    console.log("Data: ", data)
     const color = "#818cf8"
-    const imgSrc = "/aa.jpg"
-    const title = "Ethereum: All Core Devs Meetings"
-    const description = "This repository archives permanently the meetings of Ethereum core developers meetings."
+    const imgSrc = ARWEAVE_READ_LINK+data.obj?.cover
+    const title = data.obj?.podcastName
+    const description = data.obj?.description
     const descriptionLong = "This repository archives permanently the meetings of Ethereum core developers meetings. This repository archives permanently the meetings of Ethereum core developers meetings. This repository archives permanently the meetings of Ethereum core developers meetings. This repository archives permanently the meetings of Ethereum core developers meetings. This repository archives permanently the meetings of Ethereum core developers meetings."
-    const creator = "@martonlederer"
+    const creator = data.obj?.owner.length > 15 ? formatStringByLen(data.obj?.owner, 4, 4) : data.obj?.owner
     const episodeTitle = "American Rhetoric"
     const nextEpisodeTitle = "Episodes"
 
@@ -30,7 +36,7 @@ export default function PodcastId() {
             <NextEpisode 
                 containerTitle={nextEpisodeTitle} 
                 description={descriptionLong} 
-                imgSrc={""}
+                imgSrc={imgSrc}
                 creator={creator}
                 color={color}
                 title={episodeTitle}
@@ -39,4 +45,15 @@ export default function PodcastId() {
     )
 }
 
+export async function getServerSideProps(context) {
+    // Fetch data from external API
+    const { contractAddress } = getContractVariables();
+    const { params } = context
+    const podcastId = params.pid
+    const res = await axios.post(EXM_READ_LINK+contractAddress)
+    const podcasts = res.data?.podcasts
+    const data = findObjectById(podcasts, podcastId, "pid")
+
+    return { props: { data } }
+}
 //Get ServerSide Props
