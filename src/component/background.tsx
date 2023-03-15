f0442bbb67918ef22493011294360421376f7e02
Max Knivets <knivets.m@gmail.com>
2023-03-15 09:24:01 -0400

    feat: background color for creator page

--- src/component/background.tsx
+++ src/component/background.tsx
@@ -14,22 +14,23 @@ const Background: React.FC<BackgroundInterface> = ({ children }) => {
   const { pathname, asPath, query } = router;
   const [themeColor_, _] = useRecoilState(themeColor);
   const [backgroundColor_, setBackgroundColor_] = useRecoilState(backgroundColor);
-  const [podcastColor_, setPodcastColor_] = useRecoilState(podcastColor)
+  const [podcastColor_, setPodcastColor_] = useRecoilState(podcastColor);
 
   const useDefaultBackground = [
     "/",
     "/search",
     "/upload-podcast",
-  ]
+  ];
 
   useEffect(() => {
     // console.log("background.tsx useEffect");
-    if (useDefaultBackground.includes(pathname)) setBackgroundColor_(dimColorString(themeColor_, 0.1));
-    else setBackgroundColor_(dimColorString(podcastColor_, 0.1))
-  }, [pathname])
+    if (useDefaultBackground.includes(pathname)) setBackgroundColor_(dimColorString(themeColor_, 0.15));
+    else setBackgroundColor_(dimColorString(podcastColor_, 0.5));
+  }, [pathname, podcastColor_])
 
   // finish the animation for this transition later on
-  const styles = {transition: 'opacity 2.5s ease', backgroundImage: `linear-gradient(${backgroundColor_}, black, black, black)`};
+  // also do a whiter background at the bottom
+  const styles = {transition: 'opacity 2.5s ease', backgroundImage: `linear-gradient(${backgroundColor_}, black, black)`};
 
   return (
     <div className="w-screen overflow-scroll" style={true ? styles : {}}>
