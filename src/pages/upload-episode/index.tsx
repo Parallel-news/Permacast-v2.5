84c7219068ac125d73a01da4d6f236e3bc70b1ec
Max Knivets <knivets.m@gmail.com>
2023-03-14 23:32:33 -0400

    chore: uploadEpisode -> upload-episode url + translations

---
+++ src/pages/upload-episode/index.tsx
@@ -0,0 +1,26 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'react-i18next';
+import { EpisodeForm, uploadEpisodeStyling } from "../../component/uploadEpisode/uploadEpisodeTools"
+
+export default function UploadEpisode() {
+
+    const { t } = useTranslation();
+
+    return (
+        <div className={uploadEpisodeStyling}>
+            <p className="text-white text-xl">{t("uploadepisode.title")}</p>
+            <EpisodeForm />
+        </div>
+    )
+}
+
+export async function getStaticProps({ locale }) {
+    return {
+      props: {
+        ...(await serverSideTranslations(locale, [
+          'common',
+        ])),
+      },
+    }
+  }
+  
