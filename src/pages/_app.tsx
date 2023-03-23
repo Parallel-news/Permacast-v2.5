6a78372a21fe0f007d7a9aa08d16923ae4464588
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-03-23 16:25:23 -0400

    :wrench: Fix Public Folder Link for Url Previewer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,14 +19,14 @@ function App({ Component, pageProps }) {
           <Head>
             <title>Permacast</title>
             <meta name="description" content={`Permanent podcasting on Arweave. Pay once, store forever, never lose your episodes.`} />
-            <meta name="twitter:image" content={`/public/favicon.ico`} />
+            <meta name="twitter:image" content={`public/favicon.ico`} />
             <meta name="twitter:title" content={`Permacast`} />
             <meta name="twitter:url" content={`https://permacast.dev/`}></meta>
             <meta name="twitter:description" content={`Permanent podcasting on Arweave. Pay once, store forever, never lose your episodes.`} />
 
             <meta name="og:card" content="summary" />
             <meta name="description" content={`Permanent podcasting on Arweave. Pay once, store forever, never lose your episodes.`} />
-            <meta name="og:image" content={`/public/favicon.ico`} />
+            <meta name="og:image" content={`public/favicon.ico`} />
             <meta name="og:title" content={`Permacast`} />
             <meta name="og:url" content={`https://permacast.dev/`} />
             <meta name="og:description" content={`Permanent podcasting on Arweave. Pay once, store forever, never lose your episodes.`} /> 
