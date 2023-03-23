c07221095cdd460bad0079732dba0809602c8c8a
Max Knivets <knivets.m@gmail.com>
2023-03-23 08:49:52 -0400

    chore: cleanup and error fixes

--- src/component/layout.tsx
+++ src/component/layout.tsx
@@ -9,9 +9,8 @@ import Background from './background';
 
 import EpisodeQueue from './episodeQueue';
 import Fullscreen from './fullscreen';
-import VideoModal from './video_modal';
 
-import { isFullscreen, queue, currentEpisode, isPlaying, isQueueVisible, currentThemeColor } from '../atoms/index.js';
+import { isFullscreenAtom, currentThemeColor, isQueueVisibleAtom } from '../atoms/index';
 import { THEME_COLOR } from '../constants/ui';
 
 interface LayoutInterface {
@@ -22,17 +21,14 @@ const Layout: FC<LayoutInterface> = ({ children }) => {
   const { t } = useTranslation()
 
 
-  const [_isFullscreen, _setIsFullscreen] = useRecoilState(isFullscreen);
-  const [_isPlaying, _setIsPlaying] = useRecoilState(isPlaying);
-  const [_currentEpisode, _setCurrentEpisode] = useRecoilState(currentEpisode);
-  const [_queue, _setQueue] = useRecoilState(queue);
-  const [_isQueueVisible, _setQueueVisible] = useRecoilState(isQueueVisible);
+  const [_isFullscreen, _setIsFullscreen] = useRecoilState(isFullscreenAtom);
+  const [isQueueVisible, setQueueVisible] = useRecoilState(isQueueVisibleAtom);
 
   const [currentThemeColor_, setCurrentThemeColor_] = useRecoilState(currentThemeColor);
 
   useEffect(() => {
     setCurrentThemeColor_(THEME_COLOR);
-  }, [])
+  }, []);
 
   return (
     <div className="select-none h-full bg-black overflow-hidden " data-theme="permacast">
@@ -44,10 +40,11 @@ const Layout: FC<LayoutInterface> = ({ children }) => {
         </div>
         <div className="z-50">
           <div className="absolute z-50 bottom-0 right-0">
-            {_isQueueVisible && <EpisodeQueue />}
+            {isQueueVisible && <EpisodeQueue />}
           </div>
         </div>
-        {_isFullscreen && <Fullscreen episode={_currentEpisode} id={Number(_currentEpisode?.number) || 1} />}
+        {/* placeholder */}
+        {_isFullscreen && <Fullscreen episode={''} id={1} />}
         <Background>
           <div className="ml-8 pr-8 pt-9">
             <div className="mb-10">
@@ -58,7 +55,6 @@ const Layout: FC<LayoutInterface> = ({ children }) => {
             </div>
           </div>
         </Background>
-        <VideoModal/>
       </div>
     </div>
   )
