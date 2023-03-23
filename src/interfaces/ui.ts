ec98a654672ef4a5e656d9acfc617d4ab0e1602f
Max Knivets <knivets.m@gmail.com>
2023-03-23 16:27:39 -0400

    feat: new types

--- src/interfaces/ui.ts
+++ src/interfaces/ui.ts
@@ -1,23 +1,30 @@
+export type RGBstring = string;
+
 export interface RGB {
   r: number,
   g: number,
   b: number,
-}
+};
+
+export type RGBAstring = string;
+
+export type RGBorRGBAstring = RGBstring | RGBAstring;
 
 export interface RGBA {
   r: number,
   g: number,
   b: number,
   a: number,
-}
+};
 
 export interface HSL {
   h: number,
   s: number,
   l: number,
-}
+};
 
 export type RGBtoHSLInterface = (rgb: RGB) => HSL;
 
 export type replaceColorsInterface = (rgba: RGBA) => RGB;
 
+export type Classname = string;
