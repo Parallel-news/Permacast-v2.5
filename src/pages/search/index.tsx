ec2ef4a66a5f64e0f9e1f893ca11fbad92f7b25c
godzilla1980 <godzilladino1980@gmail.com>
2023-03-23 21:18:44 -0500

    :hammer: add Link Functionality to Search Results

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -9,6 +9,7 @@ import { getContractVariables } from '../../utils/contract';
 import axios from 'axios';
 import { ARWEAVE_READ_LINK, EXM_READ_LINK } from '../../constants';
 import { PodcastOption, podcastOptionHoverStyling } from '../../component/uploadEpisode/uploadEpisodeTools';
+import Link from 'next/link';
 
 export default function Search({podcasts}) {
 
@@ -17,12 +18,14 @@ export default function Search({podcasts}) {
   podcasts.forEach((podcast) => {
     podcast.name = podcast.podcastName
     podcast.category = "Podcast"
+    podcast.url = `podcast/${podcast.pid}`
     mediaArr.push(podcast)
     podcast.episodes.forEach((episode) => {
       episode.name = episode.episodeName
       episode.pid = podcast.pid
       episode.minifiedCover = podcast.minifiedCover
       episode.category = "Episode"
+      episode.url = `episode/${podcast.pid}/${episode.eid}`
       mediaArr.push(episode)
     })
   })
@@ -36,39 +39,6 @@ export default function Search({podcasts}) {
   const [_input, ] = useRecoilState(input); 
   const { t } = useTranslation();
 
-  // const filters = [
-  //   { type: "episodescount", desc: t("sorting.episodescount") },
-  //   { type: "podcastsactivity", desc: t("sorting.podcastsactivity") }
-  // ];
-  // const filterTypes = filters.map(f => f.type);
-
-  // Fetch Titles
-  useEffect(() => {
-    // const titlesContr = new AbortController();
-
-    // setTitlesLoading(true);
-    // cacheTitles({signal: titlesContr.signal})
-    // .then(tit => _setTitles(tit))
-    // .catch(e => setError(error));
-    // setTitlesLoading(false);
-
-    // return () => titlesContr.abort();
-  }, []);
-
-  // Fetch Podcasts
-  useEffect(() => {
-    // const podcastsContr = new AbortController();
-
-    // sortPodcasts(filterTypes, {signal: podcastsContr.signal})
-    // .then(sortedPodcasts => {
-    //   _setAllPodcasts(sortedPodcasts[filterTypes[_selection]]);
-    // })
-    // .catch(e => setError(e));
-
-    // return () => podcastsContr.abort();
-  }, []);
-
-
   const filteredPodcasts = _input ? 
   mediaArr.filter((item) => {
     if (_input === '') return [];
@@ -76,7 +46,7 @@ export default function Search({podcasts}) {
   })
   :
   [];
-  console.log("filteredPodcasts: ", filteredPodcasts)
+
   return (
     <div className="text-white h-full pb-80">
       {titlesLoading ? <div className="text-3xl text-white font-bold mb-6">{t("search.loading")}</div> : (
@@ -87,30 +57,20 @@ export default function Search({podcasts}) {
                 <div className="text-2xl text-white font-bold mb-6">Results</div>
                 {filteredPodcasts.length > 0 ?
                 filteredPodcasts.map((item, index) => (
-                  <div key={index} className={`mb-6 w-[25%] flex flex-row items-center ${podcastOptionHoverStyling}`}>
-                    <PodcastOption 
-                        imgSrc={ARWEAVE_READ_LINK+item.minifiedCover}
-                        title={item.name}
-                        disableClick={true}
-                    />
-                    <p className='text-neutral-400'>{item.category}</p>
-                  </div>
+                  <Link href={item.url}>
+                    <div key={index} className={`mb-6 w-[25%] flex flex-row items-center ${podcastOptionHoverStyling}`}>
+                      <PodcastOption 
+                          imgSrc={ARWEAVE_READ_LINK+item.minifiedCover}
+                          title={item.name}
+                          disableClick={true}
+                      />
+                      <p className='text-neutral-400'>{item.category}</p>
+                    </div>
+                  </Link>
                 )) 
                 : (
                   <div className="text-2xl text-white font-normal mb-12">{t("search.nopodcasts")}</div>
                 )}
-                {/* <div className="text-2xl text-white font-bold mb-6">{t("search.episodes")}</div> */}
-                {/* {filteredEpisodes.length !== 0 ? (
-                  <>
-                    {filteredEpisodes?.map((filtered, idx) => (
-                      <div key={idx} className="mb-6 p-2.5 border rounded-xl border-zinc-600">
-                        <Track episode={filtered} episodeNumber={idx} />
-                      </div>
-                    ))}
-                  </>
-                ) : (
-                  <div className="text-2xl text-white font-bold mb-12">{t("search.noepisodes")}</div>
-                )} */}
               </>
             )
             :
