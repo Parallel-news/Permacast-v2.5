cce1322ffa8bd046e6ac1835a6c6494fb3bfe007
godzilla1980 <godzilladino1980@gmail.com>
2023-03-23 20:46:38 -0500

    :hammer: buidl Search Functionality for Eps and Pods

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -6,9 +6,26 @@ import { useTranslation } from 'next-i18next';
 import { titles, allPodcasts, selection, input } from '../../atoms';
 import { useRecoilState } from 'recoil';
 import { getContractVariables } from '../../utils/contract';
+import axios from 'axios';
+import { ARWEAVE_READ_LINK, EXM_READ_LINK } from '../../constants';
+import { PodcastOption } from '../../component/uploadEpisode/uploadEpisodeTools';
 
 export default function Search({podcasts}) {
-  console.log("search podcasts: ", podcasts)
+
+  // Flatten and Modify Episodes/Podcasts into 1 Array
+  let mediaArr: any[] = []
+  podcasts.forEach((podcast) => {
+    podcast.name = podcast.podcastName
+    mediaArr.push(podcast)
+    podcast.episodes.forEach((episode) => {
+      episode.name = episode.episodeName
+      episode.pid = podcast.pid
+      episode.minifiedCover = podcast.minifiedCover
+      mediaArr.push(episode)
+    })
+  })
+
+  console.log("MA: ", mediaArr)
   const [titlesLoading, setTitlesLoading] = useState(false);
   const [error, setError] = useState("");
   const [_titles, _setTitles] = useRecoilState(titles);
@@ -50,33 +67,34 @@ export default function Search({podcasts}) {
   }, []);
 
 
-  const filteredPodcasts = _titles ? 
-  _titles.filter((p) => {
-    if (_input === '') return;
-    if (p.type === "eid") return;
-    else return p.title.toLowerCase().includes(_input.toLowerCase());
+  const filteredPodcasts = _input ? 
+  mediaArr.filter((item) => {
+    if (_input === '') return [];
+    else return item.name.toLowerCase().includes(_input.toLowerCase());
   })
   :
-  "";
-  
+  [];
+  console.log("filteredPodcasts: ", filteredPodcasts)
   return (
     <div className="text-white h-full pb-80">
-      {titlesLoading ? <div className="text-2xl text-white font-bold mb-6">{t("search.loading")}</div> : (
+      {titlesLoading ? <div className="text-3xl text-white font-bold mb-6">{t("search.loading")}</div> : (
         <div>
           {_input.length !== 0 ?
             (
               <>
-                <div className="text-2xl text-white font-bold mb-6">{t("search.podcasts")}</div>
-                {filteredPodcasts.length !== 0 ? (
-                  <>
-                    {filteredPodcasts?.map((filtered, idx) => (
-                      <div key={idx} className="mb-6 p-2.5 border rounded-xl border-zinc-600">
-                        <Track episode={filtered} includePlayButton={false} />
-                      </div>
-                    ))}
-                  </>
-                ) : (
-                  <div className="text-2xl text-white font-bold mb-12">{t("search.nopodcasts")}</div>
+                <div className="text-2xl text-white font-bold mb-6">Results</div>
+                {filteredPodcasts.length > 0 ?
+                filteredPodcasts.map((item, index) => (
+                  <div key={index} className="mb-6 w-[25%]">
+                    <PodcastOption 
+                        imgSrc={ARWEAVE_READ_LINK+item.minifiedCover}
+                        title={item.name}
+                        disableClick={false}
+                    />
+                  </div>
+                )) 
+                : (
+                  <div className="text-2xl text-white font-normal mb-12">{t("search.nopodcasts")}</div>
                 )}
                 {/* <div className="text-2xl text-white font-bold mb-6">{t("search.episodes")}</div> */}
                 {/* {filteredEpisodes.length !== 0 ? (
@@ -119,11 +137,6 @@ export async function getStaticProps({ locale }) {
     },
   }
 }
-/*
-  const { contractAddress } = getContractVariables();
-  const res = await axios.post(EXM_READ_LINK+contractAddress)
-  const podcasts = res.data?.podcasts
-
-*/
 
 
+//<Track episode={filtered} includePlayButton={false} />
