32bddd5174af0218292127a4adfb2f8fdaad44bf
Max Knivets <knivets.m@gmail.com>
2023-03-24 22:09:36 -0400

    fix: load translation file

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,13 @@
+const path = require('path')
+
+const localePath = path.resolve('./public/locales')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'uk', 'zh'],
   },
+  localePath,
+  debug: true,
+  reloadOnPrerender: process.env.IS_PROD !== 'true',
 }
