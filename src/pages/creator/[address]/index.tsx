1e77cc3f86d3d9924c1a19aef50c96fbdb0451c1
Max Knivets <knivets.m@gmail.com>
2023-03-25 03:42:07 -0400

    chore: export components

--- src/pages/creator/[address]/index.tsx
+++ src/pages/creator/[address]/index.tsx
@@ -1,181 +1,46 @@
 import axios from 'axios';
-import React, { useState, useEffect, Suspense, FC } from 'react';
-import { useTranslation } from 'next-i18next';
+import React, { useState, useEffect } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import TipButton from '../../../component/reusables/tip';
-import { Ans, Episode, EXMDevState, FullEpisodeInfo, PodcastDev } from '../../../interfaces';
+import { Ans, EXMDevState, FullEpisodeInfo, PodcastDev } from '../../../interfaces';
 import { NextPage } from 'next';
-import Image from 'next/image';
-import { dimColorString, hexToRGB, isTooLight, RGBobjectToString, RGBtoHex } from '../../../utils/ui';
-import { currentThemeColor, podcastColor } from '../../../atoms';
+import { hexToRGB, RGBobjectToString } from '../../../utils/ui';
+import { podcastColor } from '../../../atoms';
 import { useRecoilState } from 'recoil';
-import FeaturedPodcast from '../../../component/home/featuredPodcast';
-import Track from '../../../component/reusables/track';
-import Link from 'next/link';
+import { Creator404, creatorFlexCenteredStyling, creatorHeaderStyling, CreatorNames, FeaturedPodcasts, LatestEpisodes, ProfileImage, sortByDate, ViewANSButton } from '../../../component/creator';
 
-
-/**
- * Index
- * 1. Interfaces
- * 2. Stylings
- * 3. Custom Functions
- * 4. Reusable Components
- */
-
-// 1. Interfaces
-
-interface ProfileImageProps {
-  currentLabel: string;
-  avatar: string;
-  address_color: string;
-  size?: number;
-  linkToArPage?: boolean;
+// pages/blog/[slug].js
+export async function getStaticPaths() {
+  return {
+    paths: [
+      // String variant:
+      '/creator/[address].tsx',
+      // Object variant:
+      // { params: { address } },
+    ],
+    fallback: true,
+  };
 };
 
-interface CreatorNamesProps {
-  nickname: string;
-  currentLabel: string;
-};
+export async function getStaticProps(context) {
+  const { locale, params } = context;
+  const { address } = params;
+  let userInfo: Ans | null;
 
-interface ViewANSButtonProps {
-  currentLabel: string;
-};
+  if (address) {
+    const data = await axios.get(`https://ans-resolver.herokuapp.com/resolve-as-arpage/${address}`);
+    userInfo = data.data;
+  };
 
-interface FeaturedPodcastProps {
-  podcasts: PodcastDev[];
-};
-
-interface LatestEpisodesProps {
-  episodes: FullEpisodeInfo[];
-}
-
-// 2. Stylings
-const creatorHeaderStyling = `flex flex-col md:flex-row items-center justify-between`;
-const creatorFlexCenteredStyling = `flex items-center gap-x-7`;
-const creatorNicknameStyling = `select-text text-3xl font-medium tracking-wide text-white`;
-const creatorLabelStyling = `select-text text-lg font-medium text-[#828282]`;
-const creatorNicknameSmallStyling = `select-text font-medium tracking-wide text-white`;
-const creatorLabelSmallStyling = `select-text text-sm font-medium text-[#828282]`;
-
-const creatorTextHeaderTextStyling = `text-3xl font-bold text-white`;
-const podcastCarouselStyling = `w-full mt-8 carousel gap-x-12 py-3`;
-const flexCol = `flex flex-col`;
-
-// 3. Custom Functions
-
-export const resolveArDomainToArpage = (currentLabel: string) => `https://${currentLabel}.ar.page`;
-
-
-export const sortByDate = (episodes: FullEpisodeInfo[], descending = false): FullEpisodeInfo[] => {
-  return episodes.sort((a, b) => {
-    if (descending) {
-      return b.episode.uploadedAt - a.episode.uploadedAt;
-    } else {
-      return a.episode.uploadedAt - b.episode.uploadedAt;
-    }
-  });
-};
-
-// 4. Reusable Components
-export const ProfileImage: FC<ProfileImageProps> = ({ currentLabel, avatar, address_color, size, linkToArPage }) => {
-  const borderColor = address_color && isTooLight(hexToRGB(address_color), 0.6) ? "rgb(0, 0, 0)": "rgb(255, 255, 255)";
-  const hex = address_color && isTooLight(hexToRGB(address_color), 0.6) ? "#000000": "#ffffff";
-  const imageSize = size || 120;
-
-  return (
-    <Link
-      href={linkToArPage ? resolveArDomainToArpage(currentLabel): `/creator/${currentLabel}`}
-      style={{borderColor: borderColor, borderWidth: '4px', borderRadius: '999px'}}
-    >
-      {avatar && <Image width={imageSize} height={imageSize} alt={avatar} src={avatar} className="object-fit aspect-square" />}
-      {!avatar && (
-        <div style={{
-          width: imageSize,
-          height: imageSize,
-          borderRadius: '999px',
-          background: `linear-gradient(225deg, ${hex} 10%, ${address_color} 30%)`,
-        }}></div>
-      )}
-    </Link>
-  );
-};
-
-export const CreatorNamesSmall: FC<CreatorNamesProps> = ({ nickname, currentLabel }) => (
-  <div className={flexCol}>
-    <div className={creatorNicknameSmallStyling }>{nickname}</div>
-    <div className={creatorLabelSmallStyling }>@{currentLabel}</div>
-  </div>
-);
-
-export const CreatorNames: FC<CreatorNamesProps> = ({ nickname, currentLabel }) => (
-  <div className={flexCol}>
-    <div className={creatorNicknameStyling}>{nickname}</div>
-    <div className={creatorLabelStyling}>@{currentLabel}</div>
-  </div>
-);
-
-export const ViewANSButton: FC<ViewANSButtonProps> = ({ currentLabel }) => {
-  const { t } = useTranslation();
-
-  const [currentThemeColor_, setCurrentThemeColor_] = useRecoilState(currentThemeColor);
-
-  return (
-    <a
-      className="px-3 py-2 rounded-full text-sm ml-5 cursor-pointer"
-      href={`https://${currentLabel}.ar.page`}
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        backgroundColor: dimColorString(currentThemeColor_, 0.1),
-        color: currentThemeColor_,
-      }}
-    >
-      {t('creator.ans')}
-    </a>
-  );
-};
-
-const FeaturedPodcasts: FC<FeaturedPodcastProps> = ({ podcasts }) => {
-
-  const { t } = useTranslation();
-
-  return (
-    <div className="mt-8">
-      <div className={creatorTextHeaderTextStyling + " mb-8"}>{t("creator.podcasts")}</div>
-      <div>
-        {podcasts.length > 0 ? (
-          <div className={podcastCarouselStyling}>
-            {podcasts.map((podcast: PodcastDev, index: number) => 
-              <FeaturedPodcast {...podcast} key={index} />
-            )}
-          </div>
-        ): <>{t("creator.nopodcasts")}</>}
-      </div>
-    </div>
-  );
-};
-
-const LatestEpisodes: FC<LatestEpisodesProps> = ({ episodes }) => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="mt-12">
-      <div className={creatorTextHeaderTextStyling}>{t("creator.latestepisodes")}</div>
-      <div className="mt-6">
-        {episodes.map((episode: FullEpisodeInfo, episodeNumber: number) => (
-          <div className="mb-4" key={episodeNumber}>
-            <Track {...{ episode, episodeNumber }} includeDescription includePlayButton />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const Creator404: FC<{ address: string }> = ({ address }) => {
-  const { t } = useTranslation();
-
-  return <div>{address} PLACEHOLDER, will add loading here later as well</div>
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'common',
+      ])),
+      address,
+      userInfo,
+    },
+  };
 };
 
 const Creator: NextPage<{ userInfo: Ans | null, address: string }> = ({ userInfo, address }) => {
@@ -225,38 +90,5 @@ const Creator: NextPage<{ userInfo: Ans | null, address: string }> = ({ userInfo
   );
 };
 
-// pages/blog/[slug].js
-export async function getStaticPaths() {
-  return {
-    paths: [
-      // String variant:
-      '/creator/[address].tsx',
-      // Object variant:
-      // { params: { address } },
-    ],
-    fallback: true,
-  };
-};
-
-export async function getStaticProps(context) {
-  const { locale, params } = context;
-  const { address } = params;
-  let userInfo: Ans | null;
-
-  if (address) {
-    const data = await axios.get(`https://ans-resolver.herokuapp.com/resolve-as-arpage/${address}`);
-    userInfo = data.data;
-  };
-
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        'common',
-      ])),
-      address,
-      userInfo,
-    },
-  };
-};
 
 export default Creator;
