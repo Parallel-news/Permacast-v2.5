ea3046482902c471b475b1231d7a05319684b46f
Max Knivets <knivets.m@gmail.com>
2023-03-28 01:33:45 -0400

    fix: help link object

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import { FaDiscord, FaTwitter, FaTelegram } from "react-icons/fa";
+
 // Upload Podcast Constants
 export const PODCAST_NAME_MIN_LEN = 2;
 export const PODCAST_NAME_MAX_LEN = 500;
@@ -51,6 +53,34 @@ export const STR_LEN_EPISODE_DESC = 400
 // API
 export const EXM_READ_LINK = "https://api.exm.dev/read/"
 export const ARWEAVE_READ_LINK = "https://arweave.net/"
+
+// SOCIALS
+export const PERMACAST_DISCORD_URL = "https://discord.gg/cQanQVCs7G"
+export const PERMACAST_TWITTER_URL = "https://twitter.com/permacast"
+export const PERMACAST_TELEGRAM_URL = "https://t.me/permacast"
+
+const iconStyling = "w-4 h-4 text-zinc-300"
+
+export const discordObject = {
+    "name": "Discord",
+    "url": PERMACAST_DISCORD_URL,
+    "icon": <FaDiscord className={iconStyling} />
+}
+
+export const twitterObject = {
+    "name": "Twitter",
+    "url": PERMACAST_TWITTER_URL,
+    "icon": <FaTwitter className={iconStyling} />
+}
+
+export const telegramObject = {
+    "name": "Twitter",
+    "url": PERMACAST_TELEGRAM_URL,
+    "icon": <FaTelegram className={iconStyling} />
+}
+
+export const HELP_LINKS = [ discordObject, twitterObject, telegramObject ]
+
 
 // ERRORS
 export const NO_PODCAST_FOUND = "No Podcast Found."
