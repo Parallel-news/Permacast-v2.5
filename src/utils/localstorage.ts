95c9d59f06ab5b052d58a1821fb2d3d7d4a973c3
Max Knivets <knivets.m@gmail.com>
2023-03-28 04:02:56 -0400

    fix: add auto-cleanup for overflowing localstorage

--- src/utils/localstorage.ts
+++ src/utils/localstorage.ts
@@ -1,15 +1,28 @@
 // TODO: write an article about this
 
+export const TOTAL_LOCAL_STORAGE_LIMIT = 1024 * 1024 * 250; // 250MB
+
+
+
+// how to use it
+// first instantiate the class in _app.tsx (for conistent render across pages)
+// then use it in the component or UI-facing function you want to use it in
+// good example at /src/utils/ui.ts ( fetchDominantColor function )
+
 export default class LocalStorageObjectManager {
   localStorageKey: string;
+  maxSize: number;
+  // maxAge: number;
 
-  constructor(localStorageKey: string) {
+  constructor(localStorageKey: string, maxSize?: number, maxAge?: number) {
     if (!localStorageKey) throw new Error('No localStorageKey provided');
     this.localStorageKey = localStorageKey;
     try {
-      this._ensureLocalStorageIsAvailable();
+      this._ensureLocalStorageIsCallable();
       if (!localStorage.getItem(this.localStorageKey)) {
         this.setObject({});
+        this.maxSize = maxSize || 1024 * 1024 * 5; // 5MB
+        // this.maxAge = maxAge || 1000 * 60 * 60 * 24 * 7; // 7 days
       }
     } catch (error) {
       // to avoid annoying console.logs
@@ -17,7 +30,7 @@ export default class LocalStorageObjectManager {
     }
   };
 
-  _ensureLocalStorageIsAvailable() {
+  _ensureLocalStorageIsCallable() {
     try {
       if (!window?.localStorage) {
         throw new Error("Local storage is not available, looks like you're running this function from node or in private mode");
@@ -27,14 +40,27 @@ export default class LocalStorageObjectManager {
     }
   };
 
+  _ensureLocalStorageIsNotFull() {
+    try {
+      const currentSize = localStorage.getItem(this.localStorageKey).length;
+      if (currentSize > this.maxSize) {
+        console.log(`cleaning up ${this.localStorageKey} because it's over the max size of ${this.maxSize}`);
+        localStorage.setItem(this.localStorageKey, JSON.stringify({}));
+      };
+    } catch (error) {
+      console.error(error);
+    };
+  };
+
   // Getter method
   getObject() {
     try {
-      this._ensureLocalStorageIsAvailable();
+      this._ensureLocalStorageIsCallable();
+      this._ensureLocalStorageIsNotFull();
       const value = localStorage.getItem(this.localStorageKey);
       if (!value) throw new Error(`No value found for key: ${this.localStorageKey}`);
 
-      return JSON.parse(value);    
+      return JSON.parse(value);
     } catch (error) {
       console.log(error);
       return null
@@ -79,5 +105,7 @@ export default class LocalStorageObjectManager {
 };
 
 export const PODCAST_COVER_COLORS_MANAGER = 'PODCAST_COVER_COLORS_MANAGER';
+export const PODCAST_DESCRIPTION_MANAGER = 'PODCAST_DESCRIPTION_MANAGER';
 
 export const podcastCoverColorManager = new LocalStorageObjectManager(PODCAST_COVER_COLORS_MANAGER);
+export const podcastDescriptionManager = new LocalStorageObjectManager(PODCAST_DESCRIPTION_MANAGER);
