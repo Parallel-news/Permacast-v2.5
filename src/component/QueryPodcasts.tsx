320db17345ea2e2d98e7b493c9cc2c47b1b57ba8
Max Knivets <knivets.m@gmail.com>
2023-04-01 00:29:26 -0400

    fix: re-write allPodcasts queries + atom type

--- src/component/QueryPodcasts.tsx
+++ src/component/QueryPodcasts.tsx
@@ -5,20 +5,16 @@ import { allPodcasts } from '../atoms';
 import { useRecoilState } from "recoil";
 
 const QueryPodcasts: FC = () => {
-  const [_, setAllPodcasts_] = useRecoilState(allPodcasts);
+  const [allPodcasts_, setAllPodcasts_] = useRecoilState(allPodcasts);
 
   useMemo(() => {
+    if (allPodcasts_.length !== 0) return;
     console.log('initial load for QueryPodcasts.tsx');
     const fetchPodcasts = async () => {
       const exmState: EXMDevState = (await axios.get('/api/exm/read')).data;
       const { podcasts } = exmState;
-      const episodes: FullEpisodeInfo[] = podcasts
-      .map((podcast: Podcast) => podcast.episodes
-        .map((episode: Episode, index: number) => 
-          ({podcast, episode: {...episode, order: index}})))
-            .flat();
-      setAllPodcasts_(episodes);
-    };
+      setAllPodcasts_(podcasts);
+    }
     fetchPodcasts();
   }, []);
 
