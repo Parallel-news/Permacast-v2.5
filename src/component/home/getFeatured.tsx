aa9963378a1aee91eb3fc5b09a75cbd1b80bd35d
Max Knivets <knivets.m@gmail.com>
2023-04-02 01:27:41 -0400

    fix: get featured button

---
+++ src/component/home/getFeatured.tsx
@@ -0,0 +1,20 @@
+import { Dispatch, FC, SetStateAction } from "react";
+import { useTranslation } from "next-i18next";
+
+interface GetFeaturedProps {
+  onClick: () => void;
+}
+
+const GetFeaturedButtonStyling = `btn btn-secondary bg-zinc-800 hover:bg-zinc-600 transition duration-300 ease-in-out hover:text-white rounded-xl px-8 `;
+
+const GetFeatured: FC<GetFeaturedProps> = ({ onClick }) => {
+  const { t } = useTranslation();
+  
+  return (
+    <button className={GetFeaturedButtonStyling + "mt-2"} onClick={onClick}>
+      {t("home.get-featured")}
+    </button>
+  );
+};
+
+export default GetFeatured;
