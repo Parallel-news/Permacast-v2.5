d24bac2fef2c789d4a70cfc36a39679c6f9a4a25
Max Knivets <knivets.m@gmail.com>
2023-04-04 13:18:19 -0400

    feat: queryANS component

---
+++ src/component/loaders/QueryANS.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { FC, useEffect } from "react";
+
+import { allANSUsersAtom, allPodcasts } from '../../atoms';
+import { useRecoilState } from "recoil";
+import { ANS_MAPPED_STATE_URL } from '../../constants';
+
+const QueryANS: FC = () => {
+
+  const [allANSUsers, setAllANSUsers] = useRecoilState(allANSUsersAtom);
+
+  useEffect(() => {
+    if (allANSUsers.length !== 0) return;
+    console.log('initial load for QueryANS.tsx');
+    const fetchPodcasts = async () => {
+      const users = (await axios.get(ANS_MAPPED_STATE_URL)).data;
+      setAllANSUsers(users);
+    }
+    fetchPodcasts();
+  }, []);
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default QueryANS;
