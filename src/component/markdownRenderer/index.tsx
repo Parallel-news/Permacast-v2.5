5a6a05c8ff215b3bcd8f3e7d83692c976581b2ea
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-04-04 13:07:28 -0400

    :hammer: Align Left the Podcast Page

--- src/component/markdownRenderer/index.tsx
+++ src/component/markdownRenderer/index.tsx
@@ -4,10 +4,11 @@ import ReactMarkdown from 'react-markdown';
 interface MarkdownInter {
   markdownText: any;
   color?: string;
+  align?: string;
 }
 
 const MarkdownRenderer = (props: MarkdownInter) => {
-  return <ReactMarkdown className={props.color}>{props.markdownText}</ReactMarkdown>;
+  return <ReactMarkdown className={props.color+" "+props.align}>{props.markdownText}</ReactMarkdown>;
 };
 
 export default MarkdownRenderer;
