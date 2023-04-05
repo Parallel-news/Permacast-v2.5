51936cd14d5b69b2a53baf23eba3e6913e609f4a
Max Knivets <knivets.m@gmail.com>
2023-04-04 20:09:17 -0400

    fix: wrong import

--- src/component/episode/eidTools.tsx
+++ src/component/episode/eidTools.tsx
@@ -136,7 +136,7 @@ export const episodeBoxTitleDataStyling = "flex flex-row items-center space-x-3 
 export const nextEpisodeTitleStyling = "text-2xl text-neutral-300/90 font-semibold"
 export const episodeNumberStyling = "rounded-2xl bg-gray-400/30 p-2 py-1 text-[11px]"
 export const episodeTitleStyling = "select-text text-[30px] md:text-[35px] xl:text-[40px] font-medium pb-0 flex items-end text-center lg:text-start pb-0 xl:pb-4"
-xtEpisodeStyling = "w-full flex flex-col space-y-6  overflow-auto overflow-x-hidden h-[425px]"
+export const nextsqssssEpisodeStyling = "w-full flex flex-col space-y-6  overflow-auto overflow-x-hidden h-[425px]"
 export const textTruncateButtonStyling = "text-gray-400 font-bold hover:text-blue-400 transition duration-400 ease-in-out"
 export const episodeBoxStyling = "w-[98%] rounded-2xl border-2 border-gray-400/30 p-3 flex flex-row justify-between items-center bg-black"
 
