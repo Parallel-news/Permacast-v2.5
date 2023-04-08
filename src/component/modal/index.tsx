2a16e0a3edbf675f66fb7093d7b827f011ae5d1e
Max Knivets <knivets.m@gmail.com>
2023-04-08 10:21:17 -0400

    fix: modal

--- src/component/modal/index.tsx
+++ src/component/modal/index.tsx
@@ -3,16 +3,18 @@ import { FC, ReactNode } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { FADE_IN_STYLE, FADE_OUT_STYLE } from "../../constants";
-import { containerPodcastModalStyling, tipModalStyling } from "../uploadEpisode/uploadEpisodeTools";
+import { tipModalStyling } from "../uploadEpisode/uploadEpisodeTools";
 import { xMarkModalStyling } from "../tipModal";
+
 
 interface ModalProps {
   isVisible: boolean;
   setIsVisible: Dispatch<SetStateAction<boolean>>;
+  className: string;
   children: ReactNode;
 };
 
-const Modal: FC<ModalProps> = ({ isVisible, setIsVisible, children }) => {
+const Modal: FC<ModalProps> = ({ isVisible, setIsVisible, className, children }) => {
   const [showModal, setShowModal] = useState<boolean>(false)
 
   useEffect(() => {
@@ -27,7 +29,7 @@ const Modal: FC<ModalProps> = ({ isVisible, setIsVisible, children }) => {
 
   return (
     <div className={tipModalStyling + " backdrop-blur-sm"}>
-      <div className={`${containerPodcastModalStyling + " justify-between relative overflow-hidden"} ${showModal ? FADE_IN_STYLE : FADE_OUT_STYLE}`}>
+      <div className={className + ` justify-between relative overflow-hidden ` + (showModal ? FADE_IN_STYLE : FADE_OUT_STYLE)}>
         <XMarkIcon className={xMarkModalStyling} onClick={() => setIsVisible(false)} />
         {children}
       </div>
