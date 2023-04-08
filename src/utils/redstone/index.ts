4df2438e357fbf56e7e19ed945f92880fbae46b9
Max Knivets <knivets.m@gmail.com>
2023-04-08 02:24:03 -0400

    fix: conditional

--- src/utils/redstone/index.ts
+++ src/utils/redstone/index.ts
@@ -1,23 +1,23 @@
 import redstone from 'redstone-api';
 
 export async function fetchARPriceInUSD(): Promise<number> {
-  return (await redstone.getPrice("AR"))?.value;
-}
+  return (await redstone.getPrice("AR"))?.value || 0;
+};
 
 export async function fetchAR24HrChange() {
-  const msInDay = 86400000
+  const msInDay = 86400000;
   const price = await redstone.getHistoricalPrice("AR", {
     date: Date.now() - msInDay, // Timestamp in MS
   });
-  return price?.value
-}
+  return price?.value;
+};
 
 export async function calculateAR24HrDifference() {
-  const dayBeforePrice = await fetchAR24HrChange()
-  const currentPrice = await fetchARPriceInUSD()
-  return investmentPercentageGain(dayBeforePrice, currentPrice)
-}
+  const dayBeforePrice = await fetchAR24HrChange();
+  const currentPrice = await fetchARPriceInUSD();
+  return investmentPercentageGain(dayBeforePrice, currentPrice);
+};
 
 export function investmentPercentageGain(oldPrice: number, newPrice: number) {
-  return ((newPrice - oldPrice) / oldPrice) * 100
-}
+  return ((newPrice - oldPrice) / oldPrice) * 100;
+};
