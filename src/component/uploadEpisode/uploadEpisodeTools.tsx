a46c0c4a9dc9e8e3d3703884a735dfce72783356
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-04-08 09:32:51 -0400

    :wrench: Fix Select Modal Key Error

--- src/component/uploadEpisode/uploadEpisodeTools.tsx
+++ src/component/uploadEpisode/uploadEpisodeTools.tsx
@@ -495,13 +495,14 @@ export const SelectPodcastModal = (props: SelectPodcastModalInter) => {
                 <hr className={hrPodcastStyling}/>
                 {/*Options*/}
                 <div className={podcastOptionsContainer}>
-                    {props.shows && props.shows.map((item) => (
+                    {props.shows && props.shows.map((item, index) => (
                         <PodcastSelectOptions 
                             imgSrc={item.minifiedCover}
                             title={item.podcastName}
                             disable={false}
                             setPid={props.setPid}
                             pid={item.pid}
+                            key={index}
                         />
                     ))}
                     {props.shows.length === 0 && <p className="text-white text-lg text-center">{t("uploadepisode.no-shows")}</p>}
