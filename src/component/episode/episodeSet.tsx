53921b0cd278265e02da81789cd09e0cddadbaf8
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-04-12 14:37:01 -0400

    :hammer: Optimize Search Component

---
+++ src/component/episode/episodeSet.tsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { backgroundColorAtom } from "../../atoms";
+import { detectTimestampType, hasBeen10Min } from "../../utils/reusables";
+import { FullEpisodeInfo } from "../../interfaces";
+import { determinePodcastURL, fetchDominantColor, getCoverColorScheme } from "../../utils/ui";
+import { ARSEED_URL, MESON_ENDPOINT } from "../../constants";
+import { podcastIdStyling } from "./eidTools";
+import { TipModal } from "../tipModal";
+import { ShareButtons } from "../shareButtons";
+import { trimChars } from "../../utils/filters";
+
+const EpisodeBanner = React.lazy(() => import("./eidTools").then(module => ({ default: module.EpisodeBanner })));
+const EpisodeDescription = React.lazy(() => import("./eidTools").then(module => ({ default: module.EpisodeDescription })));
+const Episodes = React.lazy(() => import("./eidTools").then(module => ({ default: module.Episodes })));
+const FeaturedPodcastPlayButton = React.lazy(() => import("../home/featuredPodcastPlayButton"))
+
+interface EpisodeSetInter {
+    data: any
+}
+
+export default function EpisodeSet(props: EpisodeSetInter) {
+
+    const {data } = props
+
+    const [, setBackgroundColor_] = useRecoilState(backgroundColorAtom);
+    const [loadTipModal, setLoadTipModal] = useState<boolean>(false)
+    const [loadShareModal, setLoadShareModal] = useState<boolean>(false)
+    const [color, setColor] = useState<string>("")
+    const [baseUrl, setBaseUrl] = useState<string>("")
+
+    const [themeColor, setThemeColor] = useState<string>('');
+    const [textColor, setTextColor] = useState<string>('');
+    let index;
+    
+    if (data) {
+        // Find Episode Number
+        for(let i = 0; i < data.episodes.length; i++) {
+            //@ts-ignore
+            if(data.episodes[i].eid === data.obj.eid) {
+                index = i
+            }
+        }
+        //Serverside Results
+        detectTimestampType(data?.obj.uploadedAt)
+        let ts = new Date(detectTimestampType(data?.obj.uploadedAt) === "milliseconds" ? data?.obj.uploadedAt : data?.obj.uploadedAt* 1000);
+        const day = ts.getDate().toString().padStart(2, '0'); // get the day and add leading zero if necessary
+        const month = (ts.getMonth() + 1).toString().padStart(2, '0'); // get the month (adding 1 because getMonth() returns 0-indexed) and add leading zero if necessary
+        const year = ts.getFullYear().toString(); // get the year
+        const formattedDate = `${day}/${month}/${year}`;
+        const d = data?.obj
+        const date = formattedDate
+
+        // Assemble Player Data
+        const podcastInfo = data.podcast
+        console.log("podcastInfo: ", podcastInfo)
+        const episodes = data?.episodes
+        const cover = data.cover
+        // Create Data for Next Episode
+        //let nextEpisodeInfo: FullEpisodeInfo = {episode: {}, podcast: {}};
+        let nextEpisodeInfo = {
+            episode: null,
+            podcast: {
+                cover: '',
+                minifiedCover: '',
+                label: '',
+                author: '',
+                podcastName: '',
+                pid: '',
+            }
+        } as FullEpisodeInfo
+        
+        if(data?.episodes[index+1]) {
+            nextEpisodeInfo["episode"] = data?.episodes[index+1]
+            let podcastShort = nextEpisodeInfo["podcast"]
+            podcastShort.cover = cover
+            podcastShort.minifiedCover = cover
+            podcastShort.label = data.podcast.label
+            podcastShort.author = data.podcast.author
+            podcastShort.podcastName = data.podcast.podcastName
+            podcastShort.pid = data.podcast.pid
+        } 
+
+        const playerInfo = { playerColorScheme: themeColor, buttonColor: themeColor, accentColor: textColor, title: data.obj.episodeName, artist: data.author, cover, src: data.obj.contentTx };
+        const playButton = <FeaturedPodcastPlayButton {...{ playerInfo, podcastInfo, episodes }} />
+        useEffect(() => {
+            if(typeof window !== 'undefined') setBaseUrl(window.location.protocol + "//"+window.location.hostname+(window.location.port ? ":" + window.location.port : ""))
+            const fetchColor = async () => {
+                const dominantColor = await fetchDominantColor(data.cover);
+                const [coverColor, textColor] = getCoverColorScheme(dominantColor.rgba)
+                setColor(textColor) 
+                setBackgroundColor_(coverColor)
+                setThemeColor(coverColor);
+                setTextColor(textColor);
+            }
+            fetchColor();
+        }, []);
+
+        return (
+            <div className={podcastIdStyling}>
+            {/*Episode Cover & Info*/}
+            <EpisodeBanner 
+                title={d.episodeName}
+                imgSrc={ARSEED_URL + data?.cover}
+                color={color}
+                episodeNum={index+1}
+                date={date}
+                setLoadTipModal={() => setLoadTipModal(true)}
+                setLoadShareModal={() => setLoadShareModal(true)}
+                mediaLink={hasBeen10Min(data?.obj.uploadedAt) ? MESON_ENDPOINT+ data.obj.contentTx : ARSEED_URL + data.obj.contentTx}
+                podcastOwner={data?.obj.owner}
+                playButton={playButton}
+                podcastName={data?.podcastName}
+                pid={data?.pid}
+            />
+            {/*Episode Description*/}
+            <EpisodeDescription
+                text={d.description}
+            />
+            {/*Next Episode*/}
+            {data?.episodes[index+1] && (
+                <Episodes
+                    containerTitle={"Next Episode"} 
+                    imgSrc={ARSEED_URL + data?.cover}
+                    color={'rgb(255, 255, 255)'}
+                    episodes={[nextEpisodeInfo]}
+                    podcastId={data?.obj.pid}
+                />
+            )}
+            {loadTipModal && (
+                <TipModal
+                    to={data?.podcastName}
+                    toAddress={data?.owner} 
+                    isVisible={loadTipModal}
+                    setVisible={setLoadTipModal}
+                />
+            )}
+            {loadShareModal && (
+                <ShareButtons
+                    isVisible={loadShareModal} 
+                    setVisible={setLoadShareModal}
+                    title={d.episodeName + " - " +data.podcastName}
+                    url={`${baseUrl}/episode/${determinePodcastURL(data?.label, data?.pid)}/${trimChars(data?.obj.eid)}`}
+                />
+            )}
+        </div>
+        )
+    }
+}
--- src/pages/episode/[pid]/[eid]/index.tsx
+++ src/pages/episode/[pid]/[eid]/index.tsx
@@ -1,109 +1,19 @@
 import axios from "axios";
 import Head from "next/head";
+import React from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useEffect, useState } from "react";
-import { useRecoilState } from "recoil";
-import { backgroundColorAtom } from "../../../../atoms";
-
 import { EXM_READ_LINK, NO_PODCAST_FOUND, PAYLOAD_RECEIVED, NO_EPISODE_FOUND, ARSEED_URL, MESON_ENDPOINT } from "../../../../constants";
 import { getContractVariables } from "../../../../utils/contract";
-import { detectTimestampType, hasBeen10Min } from "../../../../utils/reusables";
-
-import { determinePodcastURL, fetchDominantColor, getCoverColorScheme } from "../../../../utils/ui";
-import { findEpisode, findPodcast, trimChars } from "../../../../utils/filters";
+import { hasBeen10Min } from "../../../../utils/reusables";
+import { findEpisode, findPodcast } from "../../../../utils/filters";
 import { checkContentTypeFromUrl } from "../../../../utils/fileTools";
-import { FullEpisodeInfo } from "../../../../interfaces";
 
-import { TipModal } from "../../../../component/tipModal";
-import { ShareButtons } from "../../../../component/shareButtons";
-//import FeaturedPodcastPlayButton from "../../../../component/home/featuredPodcastPlayButton";
-import { 
-
-    podcastIdStyling,
- } from "../../../../component/episode/eidTools";
-import React from "react";
-
- const EpisodeBanner = React.lazy(() => import("../../../../component/episode/eidTools").then(module => ({ default: module.EpisodeBanner })));
- const EpisodeDescription = React.lazy(() => import("../../../../component/episode/eidTools").then(module => ({ default: module.EpisodeDescription })));
- const Episodes = React.lazy(() => import("../../../../component/episode/eidTools").then(module => ({ default: module.Episodes })));
- const ErrorTag = React.lazy(() => import("../../../../component/episode/eidTools").then(module => ({ default: module.ErrorTag })));
- const FeaturedPodcastPlayButton = React.lazy(() => import("../../../../component/home/featuredPodcastPlayButton"))
+const ErrorTag = React.lazy(() => import("../../../../component/episode/eidTools").then(module => ({ default: module.ErrorTag })));
+const EpisodeSet = React.lazy(() => import("../../../../component/episode/episodeSet"))
 
 export default function EpisodeId({data, status, mimeType}) {
 
-    const [, setBackgroundColor_] = useRecoilState(backgroundColorAtom);
-    const [loadTipModal, setLoadTipModal] = useState<boolean>(false)
-    const [loadShareModal, setLoadShareModal] = useState<boolean>(false)
-    const [color, setColor] = useState<string>("")
-    const [baseUrl, setBaseUrl] = useState<string>("")
-
-    const [themeColor, setThemeColor] = useState<string>('');
-    const [textColor, setTextColor] = useState<string>('');
-    let index;
-    
     if (data) {
-        // Find Episode Number
-        for(let i = 0; i < data.episodes.length; i++) {
-            //@ts-ignore
-            if(data.episodes[i].eid === data.obj.eid) {
-                index = i
-            }
-        }
-        //Serverside Results
-        detectTimestampType(data?.obj.uploadedAt)
-        let ts = new Date(detectTimestampType(data?.obj.uploadedAt) === "milliseconds" ? data?.obj.uploadedAt : data?.obj.uploadedAt* 1000);
-        const day = ts.getDate().toString().padStart(2, '0'); // get the day and add leading zero if necessary
-        const month = (ts.getMonth() + 1).toString().padStart(2, '0'); // get the month (adding 1 because getMonth() returns 0-indexed) and add leading zero if necessary
-        const year = ts.getFullYear().toString(); // get the year
-        const formattedDate = `${day}/${month}/${year}`;
-        const d = data?.obj
-        const date = formattedDate
-
-        // Assemble Player Data
-        const podcastInfo = data.podcast
-        console.log("podcastInfo: ", podcastInfo)
-        const episodes = data?.episodes
-        const cover = data.cover
-        // Create Data for Next Episode
-        //let nextEpisodeInfo: FullEpisodeInfo = {episode: {}, podcast: {}};
-        let nextEpisodeInfo = {
-            episode: null,
-            podcast: {
-                cover: '',
-                minifiedCover: '',
-                label: '',
-                author: '',
-                podcastName: '',
-                pid: '',
-            }
-        } as FullEpisodeInfo
-        
-        if(data?.episodes[index+1]) {
-            nextEpisodeInfo["episode"] = data?.episodes[index+1]
-            let podcastShort = nextEpisodeInfo["podcast"]
-            podcastShort.cover = cover
-            podcastShort.minifiedCover = cover
-            podcastShort.label = data.podcast.label
-            podcastShort.author = data.podcast.author
-            podcastShort.podcastName = data.podcast.podcastName
-            podcastShort.pid = data.podcast.pid
-        } 
-
-        const playerInfo = { playerColorScheme: themeColor, buttonColor: themeColor, accentColor: textColor, title: data.obj.episodeName, artist: data.author, cover, src: data.obj.contentTx };
-        const playButton = <FeaturedPodcastPlayButton {...{ playerInfo, podcastInfo, episodes }} />
-        useEffect(() => {
-            if(typeof window !== 'undefined') setBaseUrl(window.location.protocol + "//"+window.location.hostname+(window.location.port ? ":" + window.location.port : ""))
-            const fetchColor = async () => {
-                const dominantColor = await fetchDominantColor(data.cover);
-                const [coverColor, textColor] = getCoverColorScheme(dominantColor.rgba)
-                setColor(textColor) 
-                setBackgroundColor_(coverColor)
-                setThemeColor(coverColor);
-                setTextColor(textColor);
-            }
-            fetchColor();
-        }, []);
-
         return (
             <>
                 <Head>
@@ -127,56 +37,10 @@ export default function EpisodeId({data, status, mimeType}) {
                     <meta property="og:video:type" content={mimeType} />
                     <meta property="og:video:width" content="640" />
                     <meta property="og:video:height" content="360" />
-
                 </Head>
-
-                <div className={podcastIdStyling}>
-                    {/*Episode Cover & Info*/}
-                    <EpisodeBanner 
-                        title={d.episodeName}
-                        imgSrc={ARSEED_URL + data?.cover}
-                        color={color}
-                        episodeNum={index+1}
-                        date={date}
-                        setLoadTipModal={() => setLoadTipModal(true)}
-                        setLoadShareModal={() => setLoadShareModal(true)}
-                        mediaLink={hasBeen10Min(data?.obj.uploadedAt) ? MESON_ENDPOINT+ data.obj.contentTx : ARSEED_URL + data.obj.contentTx}
-                        podcastOwner={data?.obj.owner}
-                        playButton={playButton}
-                        podcastName={data?.podcastName}
-                        pid={data?.pid}
-                    />
-                    {/*Episode Description*/}
-                    <EpisodeDescription
-                        text={d.description}
-                    />
-                    {/*Next Episode*/}
-                    {data?.episodes[index+1] && (
-                        <Episodes
-                            containerTitle={"Next Episode"} 
-                            imgSrc={ARSEED_URL + data?.cover}
-                            color={'rgb(255, 255, 255)'}
-                            episodes={[nextEpisodeInfo]}
-                            podcastId={data?.obj.pid}
-                        />
-                    )}
-                    {loadTipModal && (
-                        <TipModal
-                            to={data?.podcastName}
-                            toAddress={data?.owner} 
-                            isVisible={loadTipModal}
-                            setVisible={setLoadTipModal}
-                        />
-                    )}
-                    {loadShareModal && (
-                        <ShareButtons
-                            isVisible={loadShareModal} 
-                            setVisible={setLoadShareModal}
-                            title={d.episodeName + " - " +data.podcastName}
-                            url={`${baseUrl}/episode/${determinePodcastURL(data?.label, data?.pid)}/${trimChars(data?.obj.eid)}`}
-                        />
-                    )}
-                </div>
+                <EpisodeSet 
+                    data={data}
+                />
             </>
         )
     } else if(status === NO_PODCAST_FOUND || status === NO_EPISODE_FOUND) {
