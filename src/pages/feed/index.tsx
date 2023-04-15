5afb1068b7f66a80610eabe12035ac5e9a7f3140
0xCompressedKnowledge <0xCompressedKnowledge@proton.me>
2023-04-15 03:02:06 -0400

    refactors feed page and adds a new filter

---
+++ src/pages/feed/index.tsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import React, { FC, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { chronStatusAtom, hide0EpisodesAtom } from "../../atoms";
+import { EXM_READ_LINK, NO_SHOW } from "../../constants";
+import { getContractVariables } from "../../utils/contract";
+import { detectTimestampType } from "../../utils/reusables";
+import { Podcast } from "../../interfaces";
+
+const FeaturedPodcast = React.lazy(() => import("../../component/home/featuredPodcast"));
+const ViewDropDown = React.lazy(() => import("./viewDropDown"));
+
+const titleRow = `flex flex-row justify-between items-end mb-10 `;
+const allPodcastHeader = `text-3xl text-neutral-300/90 font-semibold pt-10 text-center md:text-start `;
+const podcastContainer = `grid grid-cols-1 justify-items-center md:justify-items-start md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1 gap-y-10 mb-10 `;
+
+interface FeedPageProps {
+  yourShows: Podcast[];
+};
+
+const FeedPage: FC<FeedPageProps> = ({ yourShows }) => {
+  const { t } = useTranslation();
+
+  const [chronStatus,] = useRecoilState<number>(chronStatusAtom);
+  const [hide0Episodes, setHide0Episodes] = useRecoilState<boolean>(hide0EpisodesAtom);
+  const [shows, setShows] = useState<Podcast[]>([]);
+
+  useEffect(() => {
+    const showsCopy = [...yourShows];
+    const shows = showsCopy.sort((a, b) => a.createdAt - b.createdAt);
+    if (chronStatus % 2) {
+      setShows(shows.reverse());
+    } else {
+      setShows(shows);
+    };
+  }, [chronStatus]);
+
+  useEffect(() => {
+    const showsCopy = [...yourShows];
+    const shows = showsCopy.filter((podcast: Podcast) => hide0Episodes ? podcast.episodes.length > 0: podcast);
+    setShows(shows);
+  }, [hide0Episodes]);
+
+  return (
+    <>
+      <div className={titleRow}>
+        {/*Header*/}
+        <div className="flex md:hidden"></div>
+        <h2 className={allPodcastHeader}>{t("feed-page.allpodcasts")}</h2>
+        <ViewDropDown />
+      </div>
+      {/*Podcasts*/}
+      <div className={podcastContainer}>
+        {shows.map((podcast: Podcast, index: number) =>
+          <FeaturedPodcast {...podcast} key={index} />
+        )}
+      </div>
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }) {
+  const { contractAddress } = getContractVariables()
+  let yourShows = null
+  let error = "";
+  try {
+    const res = await axios.get(EXM_READ_LINK + contractAddress)
+    yourShows = res.data?.podcasts;
+    yourShows.map((podcast: Podcast) => {
+      if (detectTimestampType(podcast.createdAt) === "seconds") {
+        podcast.createdAt = podcast.createdAt * 1000
+        return podcast;
+      }
+    })
+  } catch (e) {
+    error = NO_SHOW
+  }
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'common',
+      ])),
+      yourShows,
+      error
+    },
+  }
+}
+
+export default FeedPage;
---
+++ src/pages/feed/viewDropDown.tsx
@@ -0,0 +1,67 @@
+import { useTranslation } from "next-i18next";
+import { FC } from "react";
+import { useRecoilState } from "recoil";
+import { Dropdown, DropdownButtonProps } from "@nextui-org/react"
+import { EyeSlashIcon, Bars3BottomRightIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { chronStatusAtom, hide0EpisodesAtom } from "../../atoms";
+import { flexCenter } from "../../component/creator/featuredCreators";
+import { ExtendedDropdownButtonProps } from "../../component/reusables/dropdown";
+
+const ViewDropDown: FC = () => {
+
+  const { t } = useTranslation();
+
+  const [chronStatus, setChronStatus] = useRecoilState<number>(chronStatusAtom);
+  const [hide0Episodes, setHide0Episodes] = useRecoilState<boolean>(hide0EpisodesAtom);
+
+  const filterStyling = `w-12 h-12 text-zinc-600 cursor-pointer hover:bg-zinc-700 rounded-full default-no-outline-ringed default-animation px-2 `;
+  const className = 'text-white w-4 h-4 ';
+  const flexGap2 = flexCenter + `gap-x-2 `;
+
+  const SortByUploadDateButton = () => (
+    <button className={flexGap2} onClick={() => {
+      // Timeout so text doesnt change mid-click
+      setTimeout(() => setChronStatus(prev => prev + 1), 500)
+    }}>
+      <ClockIcon {...{ className }} />
+      {chronStatus % 2 ? t("feed-page.showOldest") : t("feed-page.showNewest")}
+    </button>
+  );
+
+  const Hide0EpisodePodcastsButton = () => (
+    <button className={flexGap2} onClick={() => (
+      setTimeout(() => setHide0Episodes(prev => !prev), 500)
+    )}>
+      <EyeSlashIcon {...{ className }} />
+      {hide0Episodes ? t("feed-page.show0EpisodePodcasts") : t("feed-page.hide0EpisodePodcasts")}
+    </button>
+  )
+  const menuItems = [
+    { key: "showNewest", name: "Show Newest", jsx: <SortByUploadDateButton /> },
+    { key: "hide0Episodes", name: "Hide 0 episode Podcasts", jsx: <Hide0EpisodePodcastsButton /> }
+  ];
+
+  return (
+    <Dropdown>
+      <Dropdown.Trigger>
+        <Bars3BottomRightIcon className={filterStyling} />
+      </Dropdown.Trigger>
+      <Dropdown.Menu
+        aria-label="Dynamic Actions"
+        items={menuItems}
+        className="bg-zinc-900 w-20"
+      >
+        {(item: ExtendedDropdownButtonProps) => (
+          <Dropdown.Item
+            key={item.key}
+            className='bg-zinc-900 hover:bg-zinc-700 w-60 text-white'
+          >
+            {item.jsx}
+          </Dropdown.Item>
+        )}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default ViewDropDown;
