957706dbecac06e130ec3c72fdb33074e7f5191e
godzilla1980 <godzilladino1980@gmail.com>
2023-04-18 16:11:53 -0500

    :hammer: implement Video in Page

---
+++ src/pages/tes/index.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'; 
+import { Player } from '@livepeer/react';
+import waterfallsPoster from '../../../public/telegramLogo.svg'
+
+const PosterImage = () => {
+    return (
+      <Image
+        src={waterfallsPoster}
+        alt="waterfull"
+        priority
+        placeholder="blur"
+        className="object-cover"
+        blurDataURL="https://theboutiqueadventurer.com/wp-content/uploads/2022/11/seljalandsfoss-waterfall.jpg.webp" //must be link not data:image
+      />
+    );
+  };
+
+const playbackId =
+  'bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe';
+
+//const arweaveId = "ahCH0wG_wxwmvQ_syjTZB1qxzzC3B10xJ9XdbsX1eZI"
+
+export default function WatchVideo() {
+
+  return (
+    <div className="h-[200px]">
+        <Player
+            title="Waterfalls"
+            playbackId={`${playbackId}`}
+            loop
+            autoPlay
+            muted
+            showTitle={false}
+            poster={<PosterImage />}
+            theme={{
+              colors: {
+                accent: '#00a55f',
+                background: "#fff"
+              },
+            }}
+        />
+    </div>
+  )
+}
+
+// Upload an audio asset
+// Play audio asset on command by making the player show at bottom
