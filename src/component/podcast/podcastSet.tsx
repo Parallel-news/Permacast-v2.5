70e451d04fd945eeef6dbf6842bdcdcd1a782ae0
Max Knivets <knivets.m@gmail.com>
2023-04-28 22:48:57 -0400

    fix: force white color

--- src/component/podcast/podcastSet.tsx
+++ src/component/podcast/podcastSet.tsx
@@ -49,10 +49,10 @@ export default function PodcastSet(props: podcastInter) {
             const fetchColor = async () => {
                 const dominantColor = await fetchDominantColor(cover);
                 const [coverColor, textColor] = getCoverColorScheme(dominantColor.rgba);
-                setColor(textColor)
+                setColor("rgb(255, 255, 255)")
                 setBackgroundColor(coverColor)
-                setThemeColor(coverColor)
-                setTextColor(textColor)
+                setThemeColor("rgb(255, 255, 255)")
+                setTextColor("rgb(255, 255, 255)")
             }
             fetchColor();
         }, []);
