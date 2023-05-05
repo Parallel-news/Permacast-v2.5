93a76a407cc85cacd2f303ecaa406de796692880
Max Knivets <knivets.m@gmail.com>
2023-05-05 18:44:13 -0400

    feat: check tx function

---
+++ src/utils/everpay/checkPayment.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { EverpayTxAPIResponse } from "../../interfaces/everpay";
+
+const checkPayment = async (tx: string, amount: string) => {
+  const everpayURL = "https://api.everpay.io/tx/";
+  const url = everpayURL + tx;
+
+  // example url:
+  // https://api.everpay.io/tx/0x8fac6c0c2c1c50e029a75ff0df2bcb8b643e279f1c218f9c2074f2c28f65b8ac
+
+  const res: EverpayTxAPIResponse = (await axios.get(url)).data;
+  const { status, amount: paidAmount, from, to } = res;
+  debugger;
+  if (status === "confirmed" && paidAmount >= amount) {
+    return true;
+  } else return false;
+};
+
+export default checkPayment;
