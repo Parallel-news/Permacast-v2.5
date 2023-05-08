058889ace26a663958c25ca15975a618f046c5a9
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-05-08 17:57:04 -0400

    :mag: Inspect Podcast Page on Load

--- src/pages/podcast/[pid].tsx
+++ src/pages/podcast/[pid].tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextPage } from "next";
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { ARSEED_URL, EXM_READ_LINK } from "../../constants";
 import { getContractVariables } from "../../utils/contract";
@@ -11,7 +11,7 @@ import { useRecoilState } from "recoil";
 import { loadingPage } from "../../atoms";
 
 const PodcastSet = React.lazy(() => import("../../component/podcast/podcastSet"))
-
+//import PodcastSet from "../../component/podcast/podcastSet";
 const PodcastId: NextPage<{ podcast: Podcast }> = ({ podcast }) => {
     if (podcast) {
         //State Calls Here
@@ -44,9 +44,11 @@ const PodcastId: NextPage<{ podcast: Podcast }> = ({ podcast }) => {
                     <meta property="og:image:height" content="630" />
                     <meta property="og:image:alt" content="Show Cover" />
                 </Head>
-                <PodcastSet 
-                    podcast={podcast}
-                />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <PodcastSet 
+                        podcast={podcast}
+                    />
+                </Suspense>
             </>
         )
     }
