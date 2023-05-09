354ce97c393480f2ef7eea3bc0a9ef11d77ef914
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-05-09 12:04:30 -0400

    :wrench: Rid of Loading Component

--- src/component/creator/loading.tsx
+++ src/component/creator/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
-    const loadingContainer = "w-full flex flex-col space-y-4 space-x-0 justify-center items-center md:justify-between md:space-y-0 md:flex-row md:space-x-10"
-    const loadingCircle = "h-[140px] w-[140px] rounded-full animate-pulse bg-zinc-700"
+    
     const loadingBox = "rounded-lg animate-pulse bg-zinc-700 w-[80%] h-[140px]"
+    const loadingCircle = "h-[140px] w-[140px] rounded-full animate-pulse bg-zinc-700"
+    const loadingContainer = "w-full flex flex-col space-y-4 space-x-0 justify-center items-center md:justify-between md:space-y-0 md:flex-row md:space-x-10"
+
     return (
         <div className={loadingContainer}>
             <div className={loadingCircle}>
