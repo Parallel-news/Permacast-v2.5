e1cd5be6fb32695b5ea45b7e430c14ce64b3ecad
Max Knivets <knivets.m@gmail.com>
2023-05-12 23:38:39 -0400

    fix: handle error

--- src/utils/arseeding/index.tsx
+++ src/utils/arseeding/index.tsx
@@ -48,6 +48,7 @@ export const calculateARCost = (gigabyteCost: number, bytes: number) => {
 }
 
 export const getReadableSize = (bytes: number) => {
+    if (!bytes) return '0 B';
     const sizes = ['B', 'KB', 'MB', 'GB'];
 
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
