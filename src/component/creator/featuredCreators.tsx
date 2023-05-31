b4ab889302353e647a0b6f29b57a6091bca94d5e
Max Knivets <knivets.m@gmail.com>
2023-05-31 00:34:13 -0400

    fix: featured creator component using react query

--- src/component/creator/featuredCreators.tsx
+++ src/component/creator/featuredCreators.tsx
@@ -1,19 +1,18 @@
 import axios from 'axios';
-import React, { useState, useEffect, FC } from "react";
+import React, { FC, Fragment } from "react";
 import { useTranslation } from "next-i18next";
 
 import { dimColorString } from "../../utils/ui";
 import { useRecoilState } from "recoil";
 import {
-  allPodcasts,
-  creatorsAtom,
   currentThemeColorAtom,
   loadingPage,
 } from "../../atoms";
 import Link from 'next/link';
-import { flexCol} from './';
+import { flexCol } from './';
 import { CreatorNamesSmall, ProfileImage } from './reusables';
 import { Ans, Podcast } from '../../interfaces';
+import { useQuery } from '@tanstack/react-query';
 
 /**
  * Index
@@ -28,7 +27,7 @@ export interface ViewANSButtonProps {
 
 export const flexCenter = `flex items-center `;
 export const flexFullCenter = `flex items-center ` + `justify-center `;
-export const flexCenterGap = `flex items-center `+ `gap-x-0.5 `;
+export const flexCenterGap = `flex items-center ` + `gap-x-0.5 `;
 export const borderCreatorStyling = `flex flex-row justify-between items-center p-3 my-4 w-full border-2 border-zinc-600 border rounded-2xl `;
 export const viewANSButtonStyling = `px-3 py-2 rounded-full text-sm ml-5 cursor-pointer hover:brightness-[5] default-animation outline-inherit `;
 export const creatorLoadingStyling = `bg-gray-300/30 animate-pulse w-full h-20 mb-4 rounded-full `;
@@ -39,14 +38,14 @@ export const ViewANSButton: FC<ViewANSButtonProps> = ({ currentLabel }) => {
   const [currentThemeColor] = useRecoilState(currentThemeColorAtom);
   const [, _setLoadingPage] = useRecoilState(loadingPage)
   return (
-      <Link
-        className={viewANSButtonStyling}
-        style={{ backgroundColor: dimColorString(currentThemeColor, 0.1), color: currentThemeColor, }}
-        href={`/creator/${currentLabel}`}
-        onClick={() => _setLoadingPage(true)}
-      >
-        {t("view")}
-      </Link>
+    <Link
+      className={viewANSButtonStyling}
+      style={{ backgroundColor: dimColorString(currentThemeColor, 0.1), color: currentThemeColor, }}
+      href={`/creator/${currentLabel}`}
+      onClick={() => _setLoadingPage(true)}
+    >
+      {t("view")}
+    </Link>
   );
 };
 
@@ -54,7 +53,7 @@ export const LoadingFeaturedCreators: FC = () => (
   <div className={creatorLoadingStyling}></div>
 );
 
-const Loading:FC<{ loading: boolean, dummyArray: any[] }> = ({ loading, dummyArray }) => (
+const Loading: FC<{ loading: boolean, dummyArray: any[] }> = ({ loading, dummyArray }) => (
   <>
     {loading && dummyArray.map((_, index) => (
       <LoadingFeaturedCreators key={index} />
@@ -62,86 +61,60 @@ const Loading:FC<{ loading: boolean, dummyArray: any[] }> = ({ loading, dummyArr
   </>
 );
 
-export const CreatorsMap: FC<{ creators: Ans[] }> = ({ creators }) => {
-  const creatorLimit = 10
-  let creatorCount = 0
-  return(
-    <>
-      {creators.map((creator: Ans, index: number) => {
-        if(creator.currentLabel && creatorCount < creatorLimit) {
-          creatorCount += 1
-          return (
-            <div key={index}>
-              <div className={borderCreatorStyling}>
-                <div className={`flex items-center `}>
-                  {(({ currentLabel, avatar, address_color }) => 
-                    <ProfileImage {...{currentLabel, avatar, address_color, size: 48}} squared />)
-                  (creator)}
-                  <div className={flexCol + " ml-4"}>
-                    {(({ nickname, currentLabel }) => 
-                      <CreatorNamesSmall {...{ nickname, currentLabel }} />
-                    )(creator)}
-                  </div>
-                </div>
-                <ViewANSButton currentLabel={creator.currentLabel} />
-              </div>
-            </div>
-          )
-        }
-      })}
-    </>
-  );
-}
+export const Creator: FC<{ creator: Ans }> = ({ creator }) => (
+  <div>
+    <div className={borderCreatorStyling}>
+      <div className={`flex items-center `}>
+        {(({ currentLabel, avatar, address_color }) =>
+          <ProfileImage {...{ currentLabel, avatar, address_color, size: 48 }} squared />)
+          (creator)}
+        <div className={flexCol + " ml-4"}>
+          {(({ nickname, currentLabel }) =>
+            <CreatorNamesSmall {...{ nickname, currentLabel }} />
+          )(creator)}
+        </div>
+      </div>
+      <ViewANSButton currentLabel={creator.currentLabel} />
+    </div>
+  </div>
+);
 
-export const FeaturedCreators: FC = () => {
+const fetchANSProfiles = async (addresses: string[]) => {
+  const ans: Ans[] = await Promise.all(
+    addresses.map(async (address: string) => {
+      try {
+        return (await axios.get(`/api/ans/${address}`)).data
+      } catch (e) {
+        console.log(e);
+        return {} as Ans;
+      }
+    })
+  );
+  return ans;
+};
+
+export const FeaturedCreators: FC<{ addresses: string[] }> = ({ addresses }) => {
 
   const { t } = useTranslation();
-  const [currentThemeColor, setcurrentThemeColor] = useRecoilState(currentThemeColorAtom);
-  const [allPodcasts_, setAllPodcasts_] = useRecoilState(allPodcasts);
-  const [fetchError, setFetchError] = useState<boolean>(false);
 
-  const wl = [
-    "kaYP9bJtpqON8Kyy3RbqnqdtDBDUsPTQTNUCvZtKiFI",
-    "vZY2XY1RD9HIfWi8ift-1_DnHLDadZMWrufSh-_rKF0",
-    "9J5tCNjf1EL7d-E2t8SQlcjMpwEWqMjDiGu0ktRApwY"
-  ]
+  const creatorQuery = useQuery({
+    queryKey: ["creators", addresses],
+    queryFn: async () => {
+      return { creators: await fetchANSProfiles(addresses) }
+    },
+  })
 
-  const [loading, setLoading] = useState(true);
-  const [creators, setCreators] = useRecoilState(creatorsAtom);
-
-  const episodeCounter = {}
-
-  useEffect(() => {
-    const fetchCreators = async () => {
-      setFetchError(false)
-      // fetch the creators from EXM
-
-      allPodcasts_.map((item: Podcast, index: number) => {
-        if(episodeCounter[item.owner]) {
-          episodeCounter[item.owner] += item.episodes.length
-        } else {
-          episodeCounter[item.owner] = item.episodes.length
-        }
-      })
-      
-      let sortedCounter = Object.keys(episodeCounter).sort((a, b) => episodeCounter[b] - episodeCounter[a]).splice(0, 3);
-
-      let creators = await Promise.all(
-        sortedCounter.map((address: string) => axios.get(`/api/ans/${address}`)
-      ));
-
-      setCreators(creators.map(promise => promise.data));
-      setLoading(false);
-    }
-    fetchCreators();
-  }, []);
   return (
     <div className="h-[325px] overflow-y-hidden">
       <h2 className={creatorHeadingStyling}>{t("home.featuredcreators")}</h2>
-      {loading ?
-        <Loading {...{ loading, dummyArray: wl }} />
-      :
-        <CreatorsMap {...{ creators }} />
+      {creatorQuery.isLoading ?
+        <Loading {...{ loading: true, dummyArray: [1, 1, 1] }} />
+        :
+        creatorQuery.data.creators.map((creator: Ans, index: number) => (
+          <Fragment key={index}>
+            <Creator {... { creator }} />
+          </Fragment>
+        ))
       }
     </div>
   );
