0ca3f30d5bc74abb7d36ce7429557a7e22661a67
Max Knivets <knivets.m@gmail.com>
2023-06-08 06:58:23 -0400

    fix: loading spinner removal

--- src/component/searchbar.tsx
+++ src/component/searchbar.tsx
@@ -1,20 +1,26 @@
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next';
-import { FC, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { searchInputAtom } from '../atoms';
+
+import { loadingPage, searchInputAtom } from '@/atoms/index';
 import { Icon } from './icon';
 
-export const SearchIconWrapperStyling = `flex absolute inset-y-0 left-0 items-center pl-3 pr-10 pointer-events-none `;
+export const SearchIconWrapperStyling = `flexCenter absolute inset-y-0 left-0 pl-3 pr-10 pointer-events-none `;
 export const SearchInputStyling = `block pl-10 py-3 w-full placeholder-zinc-600 focus:placeholder-zinc-400/90 rounded-full bg-zinc-900 text-zinc-400/90 outline-none focus:ring-2 focus:ring-zinc-400/90 default-animation placeholder:default-animation `;
 
 const Searchbar = () => {
   const { t } = useTranslation();
-  
+
   const router = useRouter();
 
   const [searchInput, setSearchInput] = useRecoilState(searchInputAtom);
-  
+  const [_loadingPage, _setLoadingPage] = useRecoilState(loadingPage);
+
+  useEffect(() => {
+    _setLoadingPage(false);
+  }, [])
+
   const [inputFocused, setInputFocused] = useState(false);
   const [debounceTimeout, setDebounceTimeout] = useState(null);
 
@@ -26,7 +32,7 @@ const Searchbar = () => {
     if (debounceTimeout) clearTimeout(debounceTimeout);
 
     const newTimeout = setTimeout(() => {
-      let updatedQuery = { };
+      let updatedQuery = {};
 
       // Update the query object with the new search term
       // Check if the current pathname is "/search" and if the query object already exists
@@ -54,14 +60,14 @@ const Searchbar = () => {
   };
 
   const handleFocus = () => setInputFocused(true);
-  
+
   const handleBlur = () => setInputFocused(false);
 
   return (
     <div className="relative w-full">
       <div className={SearchIconWrapperStyling}>
         <Icon
-          icon="MAGNIFY" 
+          icon="MAGNIFY"
           className={`h-5 w-5 default-animation ${inputFocused ? 'text-zinc-400/90' : 'text-zinc-600'}`}
         />
       </div>
