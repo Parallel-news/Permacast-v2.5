66e98925d0b66782f26e60cf42efa688b5fa842d
Sebastian Roa <115311276+Roaring30s@users.noreply.github.com>
2023-06-26 07:37:05 -0400

    :mag: Test Dev in Staging

--- src/component/podcast/podcastSet.tsx
+++ src/component/podcast/podcastSet.tsx
@@ -97,7 +97,7 @@ export default function PodcastSet(props: podcastInter) {
                                 </div>
                             )
                         } else {
-                            return <></>
+                            return <></> 
                         }
 
                     }) || <Loading />}
